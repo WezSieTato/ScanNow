71956a35224824f216f8a31d196ebc3808ad7c9b
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2021-08-16 22:52:43 +0200

    Initial Commit

---
+++ quick-scan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quick-scan
+//
+//  Created by Marcin on 16/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
