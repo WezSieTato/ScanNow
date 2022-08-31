0e90fdc46c0a5871909fbb969fe61460e34d2525
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2022-08-31 10:04:08 +0200

    Add scan to fastlane

--- quick-scanTests/quick_scanTests.swift
+++ quick-scanTests/quick_scanTests.swift
@@ -1,31 +1,9 @@
-//
-//  quick_scanTests.swift
-//  quick-scanTests
-//
-//  Created by Marcin on 16/08/2021.
-//
-
 @testable import quick_scan
 import XCTest
 
 class QuickScanUITests: XCTestCase {
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 }
