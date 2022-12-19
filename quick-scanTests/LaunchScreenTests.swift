d82dba0afc5f010ed44cee7ddf8f491579012121
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2022-12-20 00:26:34 +0100

    Add app icon and launch screen (#11)
    * feature: add app icon
    * feature: configure LaunchScreen

---
+++ quick-scanTests/LaunchScreenTests.swift
@@ -0,0 +1,45 @@
+@testable import quick_scan
+import SnapshotTesting
+import XCTest
+
+final class LaunchScreenSnapshotTests: XCTestCase {
+    var sut: UIViewController!
+
+    override func setUp() {
+        super.setUp()
+        let bundle = Bundle(for: AppDelegate.self)
+        let storyboard = UIStoryboard(name: "LaunchScreen", bundle: bundle)
+        sut = storyboard.instantiateInitialViewController()
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testView(
+        on config: ViewImageConfig,
+        file: StaticString = #file,
+        testName: String = #function,
+        line: UInt = #line
+    ) {
+        assertSnapshot(matching: sut, as: .image(on: config), file: file, testName: testName, line: line)
+    }
+
+    func testViewOnSmallIPhone() {
+        testView(on: .iPhoneSe)
+    }
+
+    func testViewOnIPhone8() {
+        testView(on: .iPhone8)
+    }
+
+    func testViewOnLargeIPhone() {
+        testView(on: .iPhone13ProMax)
+    }
+
+    func testViewOnIPhoneWithDarkAppearance() {
+        sut.overrideUserInterfaceStyle = .dark
+        testView(on: .iPhone8)
+    }
+}
