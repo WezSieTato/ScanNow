fdd63bc279681bf31d39a65b3d8aba33c4652e3c
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2023-02-07 13:06:36 +0100

    refactor: use DataDrivenTesting library (#39)

--- ScanNowTests/result/share/NoneFilenameStrategyTests.swift
+++ ScanNowTests/result/share/NoneFilenameStrategyTests.swift
@@ -1,31 +1,22 @@
+import DataDrivenTesting
 @testable import ScanNow
 import XCTest
 
 final class NoneFilenameStrategyTests: XCTestCase {
-    private func testFilename(
-        settingsFileName: String,
-        expectedFilename: String,
-        file: StaticString = #file,
-        line: UInt = #line
-    ) {
-        let sut = NoneFilenameStrategy()
-        let fileSettings = FileSettingsMock()
-        fileSettings.filename = settingsFileName
+    func testFilename() {
+        dataTests([
+            TestData((settingsFileName: "Example", expected: "Example")),
+            TestData((settingsFileName: "Scan", expected: "Scan")),
+            TestData((settingsFileName: "", expected: "Scan"), name: "When settingsFileName is empty"),
+        ]) { testData, _ in
 
-        let result = sut.filename(settings: fileSettings)
+            let sut = NoneFilenameStrategy()
+            let fileSettings = FileSettingsMock()
+            fileSettings.filename = testData.data.settingsFileName
 
-        XCTAssertEqual(result, expectedFilename, file: file, line: line)
-    }
+            let result = sut.filename(settings: fileSettings)
 
-    func testFilename_whenSettingsFileNameIsExample() {
-        testFilename(settingsFileName: "Example", expectedFilename: "Example")
-    }
-
-    func testFilename_whenSettingsFileNameIsScan() {
-        testFilename(settingsFileName: "Scan", expectedFilename: "Scan")
-    }
-
-    func testFilename_whenSettingsFileNameIsEmptyString() {
-        testFilename(settingsFileName: "", expectedFilename: "Scan")
+            XCTAssertEqual(result, testData.data.expected, file: testData.file, line: testData.line)
+        }
     }
 }
