8afef942a90bab106ff65994105287477833f4bd
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2023-02-07 12:44:14 +0100

    refactor: use DataDrivenTesting library

--- ScanNowTests/settings/about/VersionRowViewSnapshotTests.swift
+++ ScanNowTests/settings/about/VersionRowViewSnapshotTests.swift
@@ -1,42 +1,32 @@
+import DataDrivenTesting
 @testable import ScanNow
 import SnapshotTesting
 import SwiftUI
 import XCTest
 
 final class VersionRowViewSnapshotTests: XCTestCase {
-    func testView_whenVersionIs1_0_0andBuildNumber1() {
-        testView(version: "1.0.0", buildNumber: "1")
-    }
+    func testView() {
+        dataTests([
+            TestData((version: "1.0.0", buildNumber: "1")),
+            TestData((version: "1.0.0", buildNumber: "2")),
+            TestData((version: "2.1.3", buildNumber: "7")),
 
-    func testView_whenVersionIs1_0_0andBuildNumber2() {
-        testView(version: "1.0.0", buildNumber: "2")
-    }
+        ]) { testData, activity in
+            let versioning = VersioningMock()
+            versioning.versionNumber = testData.data.version
+            versioning.buildNumber = testData.data.buildNumber
+            let sut = Form {
+                VersionRowView(version: versioning)
+            }
 
-    func testView_whenVersionIs2_1_3andBuildNumber7() {
-        testView(version: "2.1.3", buildNumber: "7")
-    }
-
-    private func testView(
-        version: String,
-        buildNumber: String,
-        file: StaticString = #file,
-        testName: String = #function,
-        line: UInt = #line
-    ) {
-        let versioning = VersioningMock()
-        versioning.versionNumber = version
-        versioning.buildNumber = buildNumber
-        let sut = Form {
-            VersionRowView(version: versioning)
+            assertSnapshot(
+                matching: sut,
+                as: .image(layout: .fixed(width: 375, height: 90)),
+                record: false,
+                file: testData.file,
+                testName: activity.name,
+                line: testData.line
+            )
         }
-
-        assertSnapshot(
-            matching: sut,
-            as: .image(layout: .fixed(width: 375, height: 90)),
-            record: false,
-            file: file,
-            testName: testName,
-            line: line
-        )
     }
 }
